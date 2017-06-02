cf04a42f335e93c0b43c611369826167672ba381
Benson Ninh <bensonninh@gmail.com>
2017-06-01 21:26:40 -0700

    add new file to clean up component code

---
+++ src/components/AboutMe/AboutMeAssets.js
@@ -0,0 +1,9 @@
+var assets = {
+  body: `Software Engineer specialized in developing Full Stack web applicatons. \
+          My expertise lies in developing advanced React applications with NodeJS/Express as my JS run-time environment of choice, \
+          and in a variety of SQL/noSQL databases like MySQL, MongoDB, and Google Firebase. \
+          I view software as a key component to finding and building practical solutions to modern day problems.`,
+  selfie: `https://firebasestorage.googleapis.com/v0/b/web-portfolio-52c16.appspot.com/o/images%2Fselfie2015.jpg?alt=media&token=0c0a2e5f-e0a3-479d-9293-776a0e6d0d40`
+}
+
+module.exports = assets;
